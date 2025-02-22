c4e4a3af9aa9ede4c022c90040140779f379c580
Kirill Varlamov <kirill@ongrid.pro>
2025-02-22 19:45:22 +0000

    wip(ethereum): consensus layer libp2p client (tcp, swarm, noise, yamux, gossipsub)

---
+++ examples/ethereum/src/main.rs
@@ -0,0 +1,64 @@
+// The following code is a modified version of the libp2p example code
+// located at libp2p/examples/identify.rs
+
+use std::error::Error;
+
+use futures::StreamExt;
+use libp2p::identity;
+use libp2p::PeerId;
+use libp2p::{core::multiaddr::Multiaddr, identify, noise, swarm::SwarmEvent, tcp, yamux};
+use tracing_subscriber::EnvFilter;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let keypair = identity::Keypair::generate_secp256k1();
+    let peer_id = PeerId::from(keypair.public());
+
+    println!("Local Peer ID: {:?}", peer_id);
+
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .try_init();
+
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,
+            yamux::Config::default,
+        )?
+        .with_behaviour(|key| {
+            identify::Behaviour::new(identify::Config::new(
+                "/ipfs/id/1.0.0".to_string(),
+                key.public(),
+            ))
+        })?
+        .build();
+
+    // Tell the swarm to listen on all interfaces and a random, OS-assigned
+    // port.
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    // Dial the peer identified by the multi-address given as the second
+    // command-line argument, if any.
+    if let Some(addr) = std::env::args().nth(1) {
+        let remote: Multiaddr = addr.parse()?;
+        swarm.dial(remote)?;
+        println!("Dialed {addr}")
+    }
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
+            // Prints peer id identify info is being sent to.
+            SwarmEvent::Behaviour(identify::Event::Sent { peer_id, .. }) => {
+                println!("Sent identify info to {peer_id:?}")
+            }
+            // Prints out the info received via the identify event
+            SwarmEvent::Behaviour(identify::Event::Received { info, .. }) => {
+                println!("Received {info:?}")
+            }
+            _ => {}
+        }
+    }
+}
